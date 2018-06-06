86a587568404fa7fc21fc73b67278aff23d1917f
karajanyp <karajanzyp@gmail.com>
2018-06-06 11:11:13 +0800

    Update AppDelegate.m
    Add SOCKS proxy & HTTPS proxy

--- Examples/ExampleApp_iOS/AppDelegate.m
+++ Examples/ExampleApp_iOS/AppDelegate.m
@@ -35,6 +35,88 @@
     NSOperationQueue* queue = [[NSOperationQueue alloc] init];
     [queue setMaxConcurrentOperationCount:5];
     
+    #if 0
+    //socks5 proxy
+    [WebViewProxy handleRequestsWithHost:@".*" handler:^(NSURLRequest *req, WVPResponse *res) {
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        configuration.connectionProxyDictionary=@{(NSString *)kCFStreamPropertySOCKSProxyHost: @"104.233.252.165",
+                                                  (NSString *)kCFStreamPropertySOCKSProxyPort: @31601,
+                                                  (NSString *)kCFStreamPropertySOCKSUser: @"user1",
+                                                  (NSString *)kCFStreamPropertySOCKSPassword: @"passwd1"
+                                                  };
+        
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+ 
+        NSURLRequest *request = [NSURLRequest requestWithURL:req.URL];
+        
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:
+                                      ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                        NSLog(@"error = %@, response = %@", error, response);
+                                        if (error) {
+                                            return [res pipeError:error];
+                                        } else if (((NSHTTPURLResponse*)error).statusCode >= 400) {
+                                            return [res respondWithStatusCode:500 text:@"There was some sort of error :("];
+                                        } else {
+                                            [res respondWithData:data mimeType:response.MIMEType];
+                                        }
+                                      }];
+        
+        [task resume];
+    }];
+    #endif
+    
+    #if 0
+    //https proxy
+    [WebViewProxy handleRequestsWithHost:@".*" handler:^(NSURLRequest *req, WVPResponse *res) {
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+        
+        configuration.connectionProxyDictionary = @{
+                                                    @"HTTPEnable" : [NSNumber numberWithInt:1],
+                                                    (NSString *)kCFStreamPropertyHTTPProxyHost: @"98.126.23.24",
+                                                    (NSString *)kCFStreamPropertyHTTPProxyPort: @30250,
+                                                    
+                                                    @"HTTPSEnable" : [NSNumber numberWithInt:1],
+                                                    (NSString *)kCFStreamPropertyHTTPSProxyHost:@"98.126.23.24",
+                                                    (NSString *)kCFStreamPropertyHTTPSProxyPort:@30250,
+                                                    };
+
+        NSString *username = @"user1";
+        NSString *password = @"passwd1";
+        NSString *authString = [NSString stringWithFormat:@"%@:%@",
+                                username,
+                                password];
+        
+        NSData *authData = [authString dataUsingEncoding:NSUTF8StringEncoding];
+        
+        NSString *authHeader = [NSString stringWithFormat: @"Basic %@",
+                                [authData base64EncodedStringWithOptions:0]];
+        
+        [configuration setHTTPAdditionalHeaders:@{
+                                                  @"Proxy-Authorization": authHeader
+                                                  }
+        ];
+        
+        NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+
+        NSURLRequest *request = [NSURLRequest requestWithURL:req.URL];
+
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:
+                                      ^(NSData *data, NSURLResponse *response, NSError *error) {
+                                        NSLog(@"error = %@, response = %@", error, response);
+                                        if (error) {
+                                            return [res pipeError:error];
+                                        } else if (((NSHTTPURLResponse*)error).statusCode >= 400) {
+                                            return [res respondWithStatusCode:500 text:@"There was some sort of error :("];
+                                        } else {
+                                            
+                                            [res respondWithData:data mimeType:response.MIMEType];
+                                        }
+                                      }];
+        
+        [task resume];
+    }];
+    #endif
+    
     [WebViewProxy handleRequestsWithHost:@"www.google.com" path:@"/images/srpr/logo3w.png" handler:^(NSURLRequest* req, WVPResponse *res) {
         [NSURLConnection sendAsynchronousRequest:[NSURLRequest requestWithURL:req.URL] queue:queue completionHandler:^(NSURLResponse *netRes, NSData *data, NSError *netErr) {
             if (netErr) {
